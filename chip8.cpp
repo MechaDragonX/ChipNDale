bc2dda13634a63e1c4cfdbf48481651291829d5c
MechaDragonX <theofficalrv@gmail.com>
2021-03-26 18:46:30 -0700

    Add loadROM()

---
+++ chip8.cpp
@@ -0,0 +1,41 @@
+#include <cstdint>
+#include <fstream>
+#include "chip8.hpp"
+
+Chip8::Chip8() {
+    /*
+        Initialize the program counter to 0x200
+        This where all instructions will begin as the ROM contents will be loaded from here
+        0x000 to 0x1FF is reserved for system functions, so all ROM data is stored after that
+    */
+    programCounter = START_ADDRESS;
+}
+
+void Chip8::loadROM(const char* filename) {
+    // Open the file isomg a bonary stream and move the file pointer to the end
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+    if(file.is_open()) {
+        // Since the pointer is at the end, you can get the size of the file by getting the current position in the stream
+        std::streampos size = file.tellg();
+        // Alocate a buffer to hold the contents of the file
+        char* buffer = new char[size];
+
+        // Go back to he beginning of the file
+        file.seekg(0, std::ios::beg);
+        // File the buffer with the contents of the file
+        file.read(buffer, size);
+        // Close the input stream
+        file.close();
+
+        /*
+            Load the contents of the rom into memory by taking each character in the buffer and setting it to each memory address beginning, with 0x200
+            0x000 to 0x1FF is reserved for system functions, so all ROM data is stored after that
+        */
+        for(int i = 0; i < size; i++)
+            memory[START_ADDRESS + i] = buffer[i];
+
+        // Delete the buffer
+        delete[] buffer;
+    }
+}
--- chip8.hpp
+++ chip8.hpp
@@ -4,18 +4,24 @@
 #include <cstdint>
 
 class Chip8 {
+    private:
+        const unsigned int START_ADDRESS = 0x200;   // Address which the contents of ROMS are loaded into memory
     public:
-        uint8_t registers[16];          // 16 8-bit Registers
-        uint8_t memory[4096];           // Represents each of the 4096 Bytes of Memory
-        uint16_t index;                 // Index Register for Soring Memory Addresses for Operations
-        uint16_t programCounter;        // Adress of Next Instruction
-        uint16_t stack[16];             // Execution Stack
-        uint8_t stackPointer;           // Pointer Into Stack
-        uint8_t delayTimer;             // Controls Timing of CPU Cycles
-        uint8_t soundTimer;             // Controls Timing of Sound
-        uint8_t keypad[16];             // The CHIP-8 has 16 Input Keys which are represented by 0x0 to 0xF
-        uint32_t video[2048];           // Display Memory for a Display 64 pixels wide * 32 pixels tall
-        uint16_t opcode;                // An Encoded Form of the Operation and Relevant Data as a Number
+        uint8_t registers[16];                      // 16 8-bit Registers
+        uint8_t memory[4096];                       // Represents each of the 4096 bytes of memory
+        uint16_t index;                             // Index register for storing memory addresses for aperations
+        uint16_t programCounter;                    // Adress of Next Instruction
+        uint16_t stack[16];                         // Execution Stack
+        uint8_t stackPointer;                       // Pointer Into Stack
+        uint8_t delayTimer;                         // Controls Timing of CPU Cycles
+        uint8_t soundTimer;                         // Controls Timing of Sound
+        uint8_t keypad[16];                         // The CHIP-8 has 16 Input Keys which are represented by 0x0 to 0xF
+        uint32_t video[2048];                       // Display memory for a display 64 pixels wide * 32 pixels tall
+        uint16_t opcode;                            // An encoded form of the operation and relevant data as a number
+
+        Chip8();                                    // Default Constructors; Intializes Program Counter
+
+        void loadROM(const char* filename);         // Loads a ROM into memoery at the START_ADDRESS
 };
 
 #endif
