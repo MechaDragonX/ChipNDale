dd8a7bf65fb6163770d16eeeca1fbc6d4e10a6be
MechaDragonX <theofficalrv@gmail.com>
2021-03-26 18:50:05 -0700

    Fix typos in chip8.cpp

--- chip8.cpp
+++ chip8.cpp
@@ -18,18 +18,18 @@ void Chip8::loadROM(const char* filename) {
     if(file.is_open()) {
         // Since the pointer is at the end, you can get the size of the file by getting the current position in the stream
         std::streampos size = file.tellg();
-        // Alocate a buffer to hold the contents of the file
+        // Allocate a buffer to hold the contents of the file
         char* buffer = new char[size];
 
-        // Go back to he beginning of the file
+        // Go back to the beginning of the file
         file.seekg(0, std::ios::beg);
-        // File the buffer with the contents of the file
+        // Fill the buffer with the contents of the file
         file.read(buffer, size);
         // Close the input stream
         file.close();
 
         /*
-            Load the contents of the rom into memory by taking each character in the buffer and setting it to each memory address beginning, with 0x200
+            Load the contents of the rom into memory by taking each character in the buffer and setting it to each memory address, beginning with 0x200
             0x000 to 0x1FF is reserved for system functions, so all ROM data is stored after that
         */
         for(int i = 0; i < size; i++)
