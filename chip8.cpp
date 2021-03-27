701fcf4ed07dc3731c77638501e145080d3b6ad8
MechaDragonX <theofficalrv@gmail.com>
2021-03-26 21:42:13 -0700

    Implement CPU instrucitons CLS, RET, JP, CALL, and primary address ops

--- chip8.cpp
+++ chip8.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <cstdint>
+#include <cstring>
 #include <fstream>
 #include <random>
 #include "chip8.hpp"
@@ -18,7 +19,7 @@ Chip8::Chip8() : randomGenerator(std::chrono::system_clock::now().time_since_epo
     for(int i = 0; i < 80; i++)
         memory[FONTSET_START_ADDRESS + i] = fontset[i];
 
-    randomByte = std::uniform_int_distribution<uint8_t>(0, 255U);
+    randomByte = std::uniform_int_distribution<uint8_t>(0, 255u);
 }
 
 // Loads a ROM into memoery at the START_ADDRESS
@@ -50,3 +51,188 @@ void Chip8::loadROM(const char* filename) {
         delete[] buffer;
     }
 }
+
+// CPU Instructions
+
+// CLS - Clear Display
+void Chip8::OP_00E0() {
+    // Set every byte of the video buffer to zeroes
+    memset(video, 0, sizeof(video));
+}
+// RET - Return from Subroutine
+void Chip8::OP_00EE() {
+    // Move the stack pointer down one so it moves paster the previous instruction
+    stackPointer--;
+    // The make the next instruciton the current program counter
+    programCounter = stack[stackPointer];
+}
+/*
+    JP <address> - Jump to Location nnn
+    Sets program counter to nnn
+*/
+void Chip8::OP_1nnn() {
+    uint16_t address = opcode & 0x0FFFu;
+    programCounter = address;
+}
+// CALL <address> - Call Subroutine at nnn
+void Chip8::OP_2nnn() {
+    uint16_t address = opcode & 0x0FFFu;
+
+    stack[stackPointer] = programCounter;
+    stackPointer++;
+    programCounter = address;
+}
+// SE Vx, <byte> - Skip Next Instruction if Vx = kk
+void Chip8::OP_3xkk() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+    if(registers[vX] == byte)
+        programCounter += 2;
+}
+// SNE Vx, <byte> - Skip Next instruction if Vx != kk
+void Chip8::OP_4xkk() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+    if(registers[vX] != byte)
+        programCounter += 2;
+}
+// SE Vx, Vy - Skip Next Instruction if Vx = Vy
+void Chip8::OP_5xy0() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+    if(registers[vX] == registers[vY])
+        programCounter += 2;
+}
+// LD Vx, <byte> - Set Vx = kk
+void Chip8::OP_6xkk() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+    registers[vX] = byte;
+}
+// ADD Vx, <byte> - Vx += kk
+void Chip8::OP_7xkk() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+    registers[vX] += byte;
+}
+// LD Vx, Vy - Set Vx = Vy
+void Chip8::OP_8xy0() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+    registers[vX] = registers[vY];
+}
+// OR Vx, Vy - Set Vx |= Vy
+void Chip8::OP_8xy1() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+    registers[vX] |= registers[vY];
+}
+// AND Vx, Vy - Set Vx &= Vy
+void Chip8::OP_8xy2() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+    registers[vX] &= registers[vY];
+}
+// XOR Vx, Vy - Set Vx ^= Vy
+void Chip8::OP_8xy3() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+    registers[vX] ^= registers[vY];
+}
+/*
+    ADD Vx, Vy - Set Vx += Vy, Set VF = Carry
+    If sum is greater than 8 bits, VF is set to 1, otherwise 0
+    Only the lowest 8 bits of sum are kept and stored in Vx
+*/
+void Chip8::OP_8xy4() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+
+    uint16_t sum = registers[vX] + registers[vY];
+    if(sum > 255u)
+        registers[0x0F] = 1;
+    else
+        registers[0x0F] = 0;
+
+    registers[vX] = sum & 0xFFu;
+}
+/*
+    SUB Vx, Vy - Set Vx -= Vy, Set VF = Not borrow
+    If Vx > Vy, then VF is set to 1, otherwise 0
+    Then Vy is subtracted from Vx, and result's stored in Vx
+*/
+void Chip8::OP_8xy5() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+
+    if(registers[vX] > registers[vY])
+        registers[0x0F] = 1;
+    else
+        registers[0x0F] = 0;
+
+    registers[vX] -= registers[vY];
+}
+/*
+    SHR Vx - Set Vx >>= 1
+    If least-significant bit of Vx is 1, the VF is set to 1, otherwise 0
+    Then, vX is divided by 2 (aka right shift)
+*/
+void Chip8::OP_8xy6() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+
+    // Save least-significant bit in VF
+    registers[0x0F] = registers[vX] & 0x01u;
+
+    registers[vX] >>= 1;
+}
+/*
+    SUBN Vx, Vy - Set Vx = Vy - Vx, Set VF - Not Borrow
+    If Vx > Vy, then VF is set to 1, otherwise 0
+    Then Vx is subtracted from Vy, and result's stored in Vx
+*/
+void Chip8::OP_8xy7() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+
+    if(registers[vX] > registers[vY])
+        registers[0x0F] = 1;
+    else
+        registers[0x0F] = 0;
+
+    registers[vX] = registers[vY] - registers[vX];
+}
+/*
+    SHL Vx (), Vy) - Set Vx <<= 1
+    If least-significant bit of Vx is 1, the VF is set to 1, otherwise 0
+    Then, vX is multiplied by 2 (aka left shift)
+*/
+void Chip8::OP_8xyE() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+
+    registers[0x0F] = (registers[vX] & 0x80u) >> 7u;
+
+    registers[vX] <<= 1;
+}
+// SNE Vx, Vy - Skip Next Instruction if Vx != Vy
+void Chip8::OP_9xy0() {
+    uint8_t vX = (opcode & 0x0F00u) >> 8u;
+    uint8_t vY = (opcode & 0x00F0u) >> 4u;
+
+    if(registers[vX] != registers[vY])
+        programCounter += 2;
+}
+// LD I, <address> - Set I = nnn
+// JP V0, <address> - Jump to Location nnn + V0
+// RND Vx, <byte> - Set Vx = <random byte> & kk
+// DRW Vx, Vy, <nibble> - Display n-byte sprite starting at memory Location I at (Vx, Vy), Set VF = Collision
+// SKP Vx - Skip next instruction if key of Value Vx is pressed
+// SKNP Vx - Skip next instruction if key of Value Vx is not pressed
+// LD Vx, DT - Set Vx = <Delay Time Value>
+// LD Vx, L - Wait for key press, store value of key in Vx
+// LD DT, Vx - Set Delay Timer to Vx
+// LD ST, Vx - Set Sound Timer to Vx
+// ADD I, Vx - Set I += Vx
+// LD F, Vx - Set I to Locaiton of Sprite for Digit Vx
+// LD B, Vx - Store binary coded decimal representation of Vx in memory location I, I+1, I+2
+// LD [I], Vx - Store registers V0 to Vx in memeory strating at memory location I
+// LD Vx, [I] - Read registers V0 to Vx from memory strating at memory location I
--- chip8.hpp
+++ chip8.hpp
@@ -12,7 +12,7 @@ class Chip8 {
     public:
         // CPU State Information
         uint8_t registers[16];                              // 16 8-bit Registers
-        uint8_t memory[4096];                               // Represents each of the 4096 bytes of memory
+        uint8_t memory[4096];                               // Represents each of the 4096 <byte>s of memory
         uint16_t index;                                     // Index register for storing memory addresses for aperations
         uint16_t programCounter;                            // Adress of Next Instruction
         uint16_t opcode;                                    // An encoded form of the operation and relevant data as a number
@@ -25,7 +25,7 @@ class Chip8 {
         uint8_t keypad[16];                                 // The CHIP-8 has 16 Input Keys which are represented by 0x0 to 0xF
         uint32_t video[2048];                               // Display memory for a display 64 pixels wide * 32 pixels tall
 
-        uint8_t fontset[80] = {                             // 80 bytes that represents all the chracters the screen can display
+        uint8_t fontset[80] = {                             // 80 <byte>s that represents all the chracters the screen can display
             0xF0, 0x90, 0x90, 0x90, 0xF0,                       // 0
             0x20, 0x60, 0x20, 0x20, 0x70,                       // 1
             0xF0, 0x10, 0xF0, 0x80, 0xF0,                       // 2
@@ -50,6 +50,43 @@ class Chip8 {
         Chip8();                                            // Default Constructor
 
         void loadROM(const char* filename);                 // Loads a ROM into memoery at the START_ADDRESS
+
+        // CPU Instructions
+
+        void OP_00E0();                                     // CLS - Clear Display
+        void OP_00EE();                                     // RET - Return from Subroutine
+        void OP_1nnn();                                     // JP <address> - Jump to Location nnn
+        void OP_2nnn();                                     // CALL <address> - Call Subroutine at nnn
+        void OP_3xkk();                                     // SE Vx, <byte> - Skip Next Instruction if Vx = kk
+        void OP_4xkk();                                     // SNE Vx, <byte> - Skip Next instruction if Vx != kk
+        void OP_5xy0();                                     // SE Vx, Vy - Skip Next Instruction if Vx = Vy
+        void OP_6xkk();                                     // LD Vx, <byte> - Set Vx = kk
+        void OP_7xkk();                                     // ADD Vx, <byte> - Vx += kk
+        void OP_8xy0();                                     // LD Vx, Vy - Set Vx = Vy
+        void OP_8xy1();                                     // OR Vx, Vy - Set Vx |= Vy
+        void OP_8xy2();                                     // AND Vx, Vy - Set Vx &= Vy
+        void OP_8xy3();                                     // XOR Vx, Vy - Set Vx ^= Vy
+        void OP_8xy4();                                     // ADD Vx, Vy - Set Vx += Vy, Set VF = Carry
+        void OP_8xy5();                                     // SUB Vx, Vy - Set Vx -= Vy, Set VF = Not borrow
+        void OP_8xy6();                                     // SHR Vx - Set Vx >>= 1
+        void OP_8xy7();                                     // SUBN Vx, Vy - Set Vx = Vy - Vx, Set VF - Not Borrow
+        void OP_8xyE();                                     // SHL Vx (), Vy) - Set Vx <<= 1
+        void OP_9xy0();                                     // SNE Vx, Vy - Skip Next Instruction if Vx != Vy
+        void OP_Annn();                                     // LD I, <address> - Set I = nnn
+        void OP_Bnnn();                                     // JP V0, <address> - Jump to Location nnn + V0
+        void OP_Cxkk();                                     // RND Vx, <byte> - Set Vx = <random byte> & kk
+        void OP_Dxyn();                                     // DRW Vx, Vy, <nibble> - Display n-byte sprite starting at memory Location I at (Vx, Vy), Set VF = Collision
+        void OP_Ex9E();                                     // SKP Vx - Skip next instruction if key of Value Vx is pressed
+        void OP_ExA1();                                     // SKNP Vx - Skip next instruction if key of Value Vx is not pressed
+        void OP_Fx07();                                     // LD Vx, DT - Set Vx = <Delay Time Value>
+        void OP_Fx0A();                                     // LD Vx, L - Wait for key press, store value of key in Vx
+        void OP_Fx15();                                     // LD DT, Vx - Set Delay Timer to Vx
+        void OP_Fx18();                                     // LD ST, Vx - Set Sound Timer to Vx
+        void OP_Fx1E();                                     // ADD I, Vx - Set I += Vx
+        void OP_Fx29();                                     // LD F, Vx - Set I to Locaiton of Sprite for Digit Vx
+        void OP_Fx33();                                     // LD B, Vx - Store binary coded decimal representation of Vx in memory location I, I+1, I+2
+        void OP_Fx55();                                     // LD [I], Vx - Store registers V0 to Vx in memeory strating at memory location I
+        void OP_Fx65();                                     // LD Vx, [I] - Read registers V0 to Vx from memory strating at memory location I
 };
 
 #endif
